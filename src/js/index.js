2390422e2b207ddb70fb1d831f33576fb5a310d0
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-13 19:46:23 +0100

    Update the mobile menu button interactivity

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,37 @@
 import 'reset-css';
 import '../scss/styles.scss';
+
+// add event listener to the page
+document.addEventListener('click', (e) => {
+  if (e.target.dataset.btn === 'menu-open') {
+    // get the menu btn element 
+    const menuBtn = e.target.parentElement;
+    
+    // get the navbar element
+    const navbar = menuBtn.nextSibling;
+
+    // remove the class close from the menu btn
+    menuBtn.classList.remove('close');
+
+    // add the class open to the menu btn
+    menuBtn.classList.add('open');
+
+    // remove the class close from the navbar 
+    navbar.classList.remove('close');
+  } else if (e.target.dataset.btn === 'menu-close') {
+    // get the menu btn element 
+    const menuBtn = e.target.parentElement;
+
+     // get the navbar element
+    const navbar = menuBtn.nextSibling;
+
+    // remove the class open from the menu btn
+    menuBtn.classList.remove('open');
+
+    // add the class close to the menu btn
+    menuBtn.classList.add('close');
+
+    // add the class close to the navbar
+    navbar.classList.add('close');
+  }
+});
