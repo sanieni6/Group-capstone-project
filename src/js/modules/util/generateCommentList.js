238e24a1cda36394f2506a2caba17caa925ccf05
sanieni6 <sanieni6@gmail.com>
2023-03-15 15:19:54 -0500

    Add new comment counter function

--- src/js/modules/util/generateCommentList.js
+++ src/js/modules/util/generateCommentList.js
@@ -1,9 +1,10 @@
 import fetchData from './fetchData.js';
+import commentsCounter from './commentsCounter.js';
 
 const renderCommentList = (id) => {
   const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/yf6dqoUrsU3EfHXvC1i4/comments?item_id=${id}`;
   const ulElement = document.querySelector('.artworks__modal-comments-list');
-
+  const commentCounter = document.querySelector('.artworks__modal-comments-count');
   fetchData(url).then((data) => {
     if (data === 'no data') {
       const noComment = 'No comments';
@@ -12,6 +13,9 @@ const renderCommentList = (id) => {
       const renderedList = data.map((comment) => `<li class="artworks__modal-comments-item">${comment.creation_date} ${comment.username}: ${comment.comment}</li>`).join('');
       ulElement.innerHTML = renderedList;
     }
+  }).then(() => {
+    const liElements = document.querySelectorAll('.artworks__modal-comments-item');
+    commentsCounter(liElements, commentCounter);
   });
 };
 
